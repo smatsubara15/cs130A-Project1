82285d114f7e4215ae826038097d39f682b9c432
Scott Matsubara <smatsubara@cs.ucsb.edu>
2019-05-05 17:28:39 -0700

    first commit

---
+++ BloomFilter.cpp
@@ -0,0 +1,76 @@
+#include "BloomFilter.h"
+#include <algorithm>
+#include <iostream>
+
+BloomFilter::BloomFilter(int k, int m, std::string strfn, std::string intfn){
+  this->k=k;
+  this->m=m;
+  this->bits= new uint64_t[m];
+  for(int i=0;i<m;i++){
+    bits[i]=0;
+  }
+  std::transform(strfn.begin(),strfn.end(),strfn.begin(), ::tolower);
+  std::transform(intfn.begin(),intfn.end(),intfn.begin(), ::tolower);
+  if(strfn=="jenkins")
+    this->strfn=new JenkinsHash;
+  else if(strfn=="pearson")
+    this->strfn=new PearsonHash;
+  else{
+    std::cerr<<"Invalid string function"<<std::endl;
+  }
+  this->intfns=new IntegerHash*;
+  if(intfn=="division"){
+    //this->intfns=new *DivisionHash[k];
+    for(int i=0;i<k;i++){
+      intfns[i]=new DivisionHash(i,m);
+    }
+  }
+  else if(intfn=="reciprocal"){
+    //this->intfns=new *ReciprocalHash;
+    for(int i=0;i<k;i++){
+      intfns[i]=new ReciprocalHash(i,m);
+    }
+  }
+  else if(intfn=="squareroot"){
+    //this->intfns=new SquareRootHash*;
+    for(int i=0;i<k;i++){
+      intfns[i]=new SquareRootHash(i,m);
+    }
+  }
+  else{
+    std::cerr<<"Invlaed integer function"<<std::endl;
+  }
+}
+
+BloomFilter::~BloomFilter(){
+  delete strfn;
+  for(int i=0;i<k;i++){
+    delete intfns[i];
+  }
+  delete [] intfns;
+  delete [] bits;
+}
+
+void BloomFilter::insert(const std::string& value){
+  uint64_t temp=strfn->hash(value);
+  for (int i=0;i<k;i++){
+    uint64_t index=intfns[i]->hash(temp);
+    bits[index]=1;
+  }
+}
+
+bool BloomFilter::lookup(const std::string& value) const{
+  uint64_t temp=strfn->hash(value);
+  int counter=0;
+  for(int i=0;i<k;i++){
+    uint64_t index=intfns[i]->hash(temp);
+    if(bits[index]==1){
+      counter++;
+    }
+  }
+  if(counter==k)
+    return true;
+  else
+    return false;
+}
+    
---
+++ HashSet.cpp
@@ -0,0 +1,66 @@
+#include "HashSet.h"
+
+HashSet::HashSet(){
+  this->nitems=0;
+  this->nslots=100;
+  this->intfn=new SquareRootHash(1,nslots);
+  this->strfn=new JenkinsHash;
+  this->strfn2=new JenkinsHash;
+  this->slots=new std::string* [nslots];
+}
+
+HashSet::~HashSet(){
+  delete intfn;
+  delete strfn;
+  delete strfn2;
+  for(int i=0;i<nslots;i++){
+    if (slots[i]!=NULL)
+      delete slots[i];
+  }
+  delete [] slots;
+}
+
+void HashSet::insert(const std::string& value){
+  uint64_t temp=strfn->hash(value);
+  uint64_t index=intfn->hash(temp);
+  double loadFactor=static_cast<double>(nitems)/static_cast<double>(nslots);
+  if (loadFactor>0.5)
+    rehash();
+  while(slots[index]!=NULL && *slots[index]!=value){
+    index++;
+    index=index%nslots;
+  }
+  if(slots[index]==NULL){
+    nitems++;
+    slots[index]=new std::string(value);
+  }
+}
+
+void HashSet::rehash(){
+  std::string tempArray[nitems];
+  int index=0;
+  for(int i=0;i<nslots;i++){
+    if(slots[i]!=NULL){
+      tempArray[index]=*slots[i];
+      index++;
+    }
+  }
+  this->nslots=(this->nslots)*2;
+  for(int i=0;i<nitems;i++){
+    insert(tempArray[i]);
+  }
+}
+
+bool HashSet::lookup(const std::string& value) const{
+    uint64_t temp=strfn->hash(value);
+    uint64_t index=intfn->hash(temp);
+    while(slots[index]!=NULL){
+      if(*slots[index]==value)
+	return true;
+      else{
+	index++;
+	index=index%nslots;
+      }
+    }
+    return false;
+}
