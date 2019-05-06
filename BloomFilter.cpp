4466e3f267addcf97190e5ece606107ae806c7f2
Scott Matsubara <smatsubara@cs.ucsb.edu>
2019-05-05 21:56:25 -0700

    Implemented

--- BloomFilter.cpp
+++ BloomFilter.cpp
@@ -11,37 +11,36 @@ BloomFilter::BloomFilter(int k, int m, std::string strfn, std::string intfn){
   }
   std::transform(strfn.begin(),strfn.end(),strfn.begin(), ::tolower);
   std::transform(intfn.begin(),intfn.end(),intfn.begin(), ::tolower);
-  if(strfn=="jenkins")
+  //if(strfn=="jenkins")
     this->strfn=new JenkinsHash;
-  else if(strfn=="pearson")
-    this->strfn=new PearsonHash;
-  else{
-    std::cerr<<"Invalid string function"<<std::endl;
-  }
+    //else if(strfn=="pearson")
+    //this->strfn=new PearsonHash;
+    //else{
+    //std::cerr<<"Invalid string function"<<std::endl;
+    //}
   this->intfns=new IntegerHash*[k];
-  if(intfn=="division"){
+  //if(intfn=="division"){
     //this->intfns=new DivisionHash*[k];
     for(int i=0;i<k;i++){
       intfns[i]=new DivisionHash(i,m);
-    }
-  }
-  else if(intfn=="reciprocal"){
+      //}
+      //}
+      //else if(intfn=="reciprocal"){
     //this->intfns=new ReciprocalHash*[k];
-    for(int i=0;i<k;i++){
-      intfns[i]=new ReciprocalHash(i,m);
-    }
-  }
-  else if(intfn=="squareroot"){
+    //for(int i=0;i<k;i++){
+      //intfns[i]=new ReciprocalHash(i,m);
+      //}
+      //}
+      //else if(intfn=="squareroot"){
     //this->intfns=new SquareRootHash*[k];
-    for(int i=0;i<k;i++){
-      intfns[i]=new SquareRootHash(i,m);
+    //for(int i=0;i<k;i++){
+      // intfns[i]=new SquareRootHash(i,m);
+      //}
+      //}
+      //else{
+      //std::cerr<<"Invlaed integer function"<<std::endl;
+      }
     }
-  }
-  else{
-    std::cerr<<"Invlaed integer function"<<std::endl;
-  }
-}
-
 BloomFilter::~BloomFilter(){
   delete strfn;
   for(int i=0;i<k;i++){
